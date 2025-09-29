6bf50236d45d0c61a0498718040834ebf517daa9
kristiyan krastev <rumenovkristiyan@gmail.com>
2025-09-29 20:08:43 +0000

    fixing versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,24 @@ version = ">= 3.6.0"
 }
 
 provider "azurerm" {
-  features {}
+  features {
+        resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+}
+variable "allow_rg_destroy" {
+  type    = bool
+  default = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.name_prefix}-rg"
+  location = var.location
+
+  lifecycle {
+    prevent_destroy = var.allow_rg_destroy ? false : true
+  }
 }
