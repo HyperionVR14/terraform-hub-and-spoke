f0678238d57122d276bf3eb4fc6fbe12a5cdfe88
kristiyan krastev <rumenovkristiyan@gmail.com>
2025-10-01 17:21:17 +0000

    Fixing destroy functionality.

--- modules/keyvault/variables.tf
+++ modules/keyvault/variables.tf
@@ -4,3 +4,17 @@ variable "location" { type = string }
 variable "vnet_id" { type = string }
 variable "subnet_id" { type = string }
 variable "vnet_links" { type = map(string) }
+
+#terraform destroy functionality:
+
+variable "protect_destroy" {
+  description = "Hard-stop against destroy. by default true for prod."
+  type        = bool
+  default     = true
+}
+
+variable "destroy_breakglass" {
+  description = "Manual override for temp unlock (particular string in sdefined in the root)."
+  type        = string
+  default     = ""
+}
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -3,3 +3,17 @@ variable "resource_group_name" { type = string }
 variable "location" { type = string }
 variable "address_space" { type = list(string) }
 variable "subnet_name" { type = string }
+
+#terraform destroy functionality:
+#
+#variable "protect_destroy" {
+#  description = "Hard-stop against destroy. by default true for prod."
+#  type        = bool
+#  default     = true
+#}
+#
+#variable "destroy_breakglass" {
+#  description = "Manual override for temp unlock (particular string in sdefined in the root)."
+#  type        = string
+#  default     = ""
+#}
